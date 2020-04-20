603aa1ac26c8e72bb46235eef0b288b95b780f26
rogihyun <srlgus2002v@naver.com>
2020-04-20 19:08:13 +0900

    0420

--- bitcamp-spring-webmvc/src/main/java/bitcamp/app1/Controller02_1.java
+++ bitcamp-spring-webmvc/src/main/java/bitcamp/app1/Controller02_1.java
@@ -6,21 +6,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller 
+@Controller
 @RequestMapping("/c02_1")
 public class Controller02_1 {
 
   // 테스트 방법:
-  // => http://localhost:8080/java-spring-webmvc/html/app1/c02_1.html
-  
-  @RequestMapping(method = RequestMethod.GET) // GET 요청일 때만 호출된다. 
-  @ResponseBody 
+  // => http://localhost:9999/java-spring-webmvc/html/app1/c02_1.html
+
+  @RequestMapping(method = RequestMethod.GET) // GET 요청일 때만 호출된다.
+  @ResponseBody
   public String handler1() {
     return "get";
   }
-  
-  @RequestMapping(method = RequestMethod.POST) // POST 요청일 때만 호출된다. 
-  @ResponseBody 
+
+  @RequestMapping(method = RequestMethod.POST) // POST 요청일 때만 호출된다.
+  @ResponseBody
   public String handler2() {
     return "post";
   }
--- bitcamp-spring-webmvc/src/main/java/bitcamp/app1/Controller03_1.java
+++ bitcamp-spring-webmvc/src/main/java/bitcamp/app1/Controller03_1.java
@@ -6,31 +6,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller 
+@Controller
 @RequestMapping("/c03_1")
 public class Controller03_1 {
 
   // 테스트 방법:
   // => http://localhost:8080/java-spring-webmvc/app1/c03_1?name=kim
-  @GetMapping(params="name") 
-  @ResponseBody 
+  @GetMapping(params="name")
+  @ResponseBody
   public String handler1() {
     return "handler1";
   }
-  
+
   //테스트 방법:
   // => http://localhost:8080/java-spring-webmvc/app1/c03_1?age=20
-  @GetMapping(params="age") 
-  @ResponseBody 
+  @GetMapping(params="age")
+  @ResponseBody
   public String handler2() {
     return "handler2";
   }
-  
+
   //테스트 방법:
   // => http://localhost:8080/java-spring-webmvc/app1/c03_1?name=kim&age=20
-  @GetMapping(params={"age","name"}) 
-  @ResponseBody 
+  @GetMapping(params={"age","name"})
+  @ResponseBody
   public String handler3() {
     return "handler3";
+  }
+
+//테스트 방법:
+  // => http://localhost:8080/java-spring-webmvc/app1/c03_1?name=kim&age=20
+  @GetMapping
+  @ResponseBody
+  public String handler4() {
+    return "handler4";
   }
 }
